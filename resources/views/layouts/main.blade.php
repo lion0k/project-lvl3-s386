8bb6a690573afc96f4744ba5e004cfb30e0b5ec7
lion0k <lion_ok@mail.ru>
2019-01-24 14:21:56 +0300

    change main view

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,29 +8,34 @@
     <meta name="author" content="">
     <title>Page Analyzer</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css?family=Montserrat:500" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Titillium+Web:700,900" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="/css/style.css" />
 </head>
 
 <body>
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <h5 class="my-0 mr-md-auto font-weight-normal"><a class="p-2 text-dark" href="{{ route('index') }}">Page Analyzer</a></h5>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-            <a class="nav-item nav-link" href={{ route('domains.index') }}>Domains</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <h5 class="my-0 mr-md-auto font-weight-normal">
+            <a class="navbar-brand" href="{{ route('index') }}">
+                <img src="/img/hexlet_logo.svg" width="40" height="40" alt="logo" class="d-inline-block align-center">Page Analyzer
+            </a>
+        </h5>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMain"
+                aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarMain">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href={{ route('domains.index') }}>Domains</a>
+            </div>
         </div>
+    </nav>
+
+    <div role="main" class="container">
+        @yield('content')
     </div>
-</nav>
 
-<div role="main" class="container">
-    @yield('content')
-</div>
-
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 </body>
 </html>
